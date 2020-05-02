81d40883308a31ce17664d40c4ee812128194ed5
MacMohammed <macmohammed@gmail.com>
2020-05-02 09:16:09 -0300

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Generos from "./components/Generos/Genero";
+import NovoGenero from "./components/Generos/NovoGenero";
+import EditarGenero from "./components/Generos/EditarGenero";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Series from "./components/Series/Serie";
+import NovaSerie from "./components/Series/NovaSerie";
+import InfoSerie from "./components/Series/InfoSerie";
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/generos" exact component={Generos} />
+          <Route path="/generos/novo" exact component={NovoGenero} />
+          <Route path="/generos/:id" exact component={EditarGenero} />
+          <Route path="/series" exact component={Series} />
+          <Route path="/series/novo" exact component={NovaSerie} />
+          <Route path="/series/:id" exact component={InfoSerie} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
